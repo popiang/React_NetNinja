53eea9d1366cf5a888a23bbb639c9a8934301085
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-01-27 06:59:15 +0800

    react

--- Tuts/my-first-react-app/src/App.js
+++ Tuts/my-first-react-app/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import React, { useState } from "react";
 import Title from './components/Title';
+import Modal from './components/Modal';
 
 function App() {
+	const [showModal, setShowModal] = useState(false);
     const [showEvents, setShowEvents] = useState(true);
     const [events, setEvents] = useState([
         { title: "mario's birthday bash", id: 1 },
@@ -10,7 +12,7 @@ function App() {
         { title: "race on moo moo farm", id: 3 },
     ]);
 
-	console.log(showEvents);
+	console.log(showModal);
 
     const handleClick = (id) => {
         setEvents((prevEvents) => {
@@ -21,32 +23,66 @@ function App() {
         console.log(id);
     };
 
+	const handleClose = () => {
+		setShowModal(false);
+	}
+
+	const handleOpen = () => {
+		setShowModal(true);
+	}
+
 	const subtitle = "All the latest events in Marioland";
 
     return (
         <div className="App">
-			<Title title="Events in Your Area" subtitle={subtitle}/>
+            <Title title="Events in Your Area" subtitle={subtitle} />
 
             {showEvents && (
-				<div>
-					<button onClick={() => setShowEvents(false)}>hide events</button>
-				</div>
-			)}
-            {!showEvents && (
-				<div>
-					<button onClick={() => setShowEvents(true)}>show events</button>
-				</div>
-			)}
-            {showEvents && events.map((event, index) => (
-                <React.Fragment key={event.id}>
-                    <h2>
-                        {index + 1} - {event.title}
-                    </h2>
-                    <button onClick={() => handleClick(event.id)}>
-                        delete event
+                <div>
+                    <button onClick={() => setShowEvents(false)}>
+                        hide events
                     </button>
-                </React.Fragment>
-            ))}
+                </div>
+            )}
+            {!showEvents && (
+                <div>
+                    <button onClick={() => setShowEvents(true)}>
+                        show events
+                    </button>
+                </div>
+            )}
+            {showEvents &&
+                events.map((event, index) => (
+                    <React.Fragment key={event.id}>
+                        <h2>
+                            {index + 1} - {event.title}
+                        </h2>
+                        <button onClick={() => handleClick(event.id)}>
+                            delete event
+                        </button>
+                    </React.Fragment>
+                ))}
+
+            {/* <Modal>
+                <h2>10% Off Coupon Code!!</h2>
+                <p>Use the code NINJA10 at the checkout</p>
+            </Modal> */}
+            <div>
+                <button onClick={handleOpen}>show modal</button>
+            </div>
+
+            {showModal && (
+                <Modal handleClose={handleClose}>
+                    <h2>Terms and Conditions</h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Necessitatibus repellat exercitationem
+                        repellendus, minus id doloremque, placeat quas
+                        laudantium iure autem tempora sunt libero corrupti velit
+                        veniam ipsam eos nam numquam.
+                    </p>
+                </Modal>
+            )}
         </div>
     );
 }
