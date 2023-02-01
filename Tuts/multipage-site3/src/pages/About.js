fd07d47e7b688432486a494a4f9dff4d9cbb3dac
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-02-01 14:45:17 +0800

    multipage-site3

---
+++ Tuts/multipage-site3/src/pages/About.js
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+        <div>
+            <h2>About Page</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus, quo quidem eaque, ratione corrupti cupiditate
+                accusamus hic molestias voluptatum laboriosam reprehenderit
+                adipisci ut excepturi eum. Nesciunt quas esse consectetur
+                incidunt?
+            </p>
+        </div>
+    );
+}
---
+++ Tuts/multipage-site3/src/pages/Contact.js
@@ -0,0 +1,13 @@
+export default function Contact() {
+    return (
+        <div>
+            <h2>Contact Page</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+                sequi fuga quam quibusdam commodi voluptas, officiis deserunt
+                magnam! Animi aliquam ipsa veritatis vero ipsam ad, commodi eius
+                optio debitis quo.
+            </p>
+        </div>
+    );
+}
