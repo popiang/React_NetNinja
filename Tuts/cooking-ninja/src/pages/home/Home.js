25326ade0a737cbfbe58b56d7a755ec632337877
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-02-04 11:19:31 +0800

    Cooking Ninja

---
+++ Tuts/cooking-ninja/src/pages/home/Home.js
@@ -0,0 +1,23 @@
+import { useFetch } from "../../hooks/useFetch";
+
+// styles
+import "./Home.css";
+
+// components
+import RecipeList from "../../components/RecipeList";
+
+export default function Home() {
+    const {
+        data: recipes,
+        isPending,
+        error,
+    } = useFetch("http://localhost:3000/recipes");
+
+    return (
+        <div className="home">
+            {isPending && <p className="loading">Loading...</p>}
+            {error && <p className="error">{error}</p>}
+            {recipes && <RecipeList recipes={recipes} />}
+        </div>
+    );
+}
