5a9f3b04255cb869d9d9f007e6a48d882a49bbda
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-03-02 00:03:02 +0800

    section 14

--- Tuts/my-money/src/App.js
+++ Tuts/my-money/src/App.js
@@ -1,11 +1,32 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// components
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
+// styles
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+		<div className="App">
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/signup">
+						<Signup />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
-export default App
+export default App;
+ 
---
+++ Tuts/my-money/src/pages/home/Home.js
@@ -0,0 +1,7 @@
+import styles from './Home.module.css';
+
+export default function home() {
+	return (
+		<div>home</div>
+	)
+}
